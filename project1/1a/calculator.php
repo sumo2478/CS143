8906f28bef39dc5d45d621a96a120a50639a54db
Collin Yen <collinyen7@gmail.com>
2014-01-13 16:36:12 -0800

    Evaluation Change
    Changed eval() to not execute arbitrary code
    Now handles white spaces
    Now handles “int- -int” operations

--- project1/1a/calculator.php
+++ project1/1a/calculator.php
@@ -11,6 +11,12 @@
 	{
 		$expr = $_GET['expr'];
 
+		// Remove all spaces
+		$expr = str_replace(" ", "", $expr);
+
+		// Replace instances of -- with +
+		$expr = str_replace("--", "+", $expr);
+
 		// Check for invalid characters
 		$error_expr = '/[^0-9\*\-\+\/]/';
 		if (preg_match($error_expr, $expr)) {
@@ -18,9 +24,39 @@
 			return;
 		}
 
-		// Handle all the multiplications and divisions
-		eval("\$result = $expr;");
+        // Handle all the multiplications and divisions
+        $multdiv_pattern = '/\-?\d+\.*\d*(\*|\/)\-?\d+\.*\d*/';
 
-		print("Answer: ".$result."\n");
+        preg_match($multdiv_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
+
+        while($matches)
+        {
+                $equation = $matches[0][0];
+                eval("\$result = $equation;");
+                $count = 1;
+                $expr = str_replace($matches[0][0], $result,$expr, $count);
+
+                preg_match($multdiv_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);                        
+        }
+
+        // Handle addition and subtraction
+        $addsub_pattern = '/\-?\d+\.*\d*(\+|\-)\-?\d+\.*\d*/';
+
+        preg_match($addsub_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
+
+        while($matches)
+        {
+                $equation = $matches[0][0];
+                eval("\$result = $equation;");
+                $count = 1;
+                $expr = str_replace($matches[0][0], $result,$expr, $count);
+
+                preg_match($addsub_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);                        
+        }
+
+        print("Answer: ".$result."\n");
 	}
 ?>
+
+                
+
