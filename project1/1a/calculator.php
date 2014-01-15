d184e901f88397ac95f9f2c4291852f6dabbac6d
Alex Guo <aguo91@gmail.com>
2014-01-14 17:11:23 -0800

    Added catch for division by zero

--- project1/1a/calculator.php
+++ project1/1a/calculator.php
@@ -6,33 +6,47 @@
 </p>
 
 <?php 
-	// Retrieve the expression from the form if available
-	if ($_GET['expr'])
-	{
-		$expr = $_GET['expr'];
+        // Retrieve the expression from the form if available
+        if ($_GET['expr'])
+        {
+                $expr = $_GET['expr'];
 
-		// Remove all spaces
-		$expr = str_replace(" ", "", $expr);
+                // Remove all spaces
+                $expr = str_replace(" ", "", $expr);
 
-		// Replace instances of -- with +
-		$expr = str_replace("--", "+", $expr);
+                // Replace instances of -- with +
+                $expr = str_replace("--", "+", $expr);
 
-		// Check for invalid characters
-		$error_expr = '/[^0-9\*\-\+\/]/';
-		if (preg_match($error_expr, $expr)) {
-			print("Invalid input: ".$expr."\n");
-			return;
+                // Check for invalid characters
+                $error_expr = '/[^0-9\*\-\+\/]/';
+                if (preg_match($error_expr, $expr)) {
+                        print("Invalid input: ".$expr."\n");
+                        return;
+                }
+
+        $zero = FALSE;
+
+		function error_z($errno, $errstr, $errfile, $errline) {
+		    if ($errstr == "Division by zero") {
+		        global $zero;
+		        $zero = TRUE;
+		    }
 		}
+
+		set_error_handler('error_z');
 
         // Handle all the multiplications and divisions
         $multdiv_pattern = '/\-?\d+\.*\d*(\*|\/)\-?\d+\.*\d*/';
 
         preg_match($multdiv_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
-
         while($matches)
         {
                 $equation = $matches[0][0];
                 eval("\$result = $equation;");
+                if ($zero == TRUE) { 
+				    echo "Invalid Input: Division by zero error!";
+				    return;
+				}
                 $count = 1;
                 $expr = str_replace($matches[0][0], $result,$expr, $count);
 
@@ -55,8 +69,5 @@
         }
 
         print("Answer: ".$result."\n");
-	}
+        }
 ?>
-
-                
-
