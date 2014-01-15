082ac7cf365cb61287e9591972b1c2a02395632b
Collin Yen <collinyen7@gmail.com>
2014-01-14 17:46:21 -0800

    Fixed bugs with not being able to input a decimal point and single digits without an expression returning incorrect results

--- project1/1a/calculator.php
+++ project1/1a/calculator.php
@@ -9,65 +9,66 @@
         // Retrieve the expression from the form if available
         if ($_GET['expr'])
         {
-                $expr = $_GET['expr'];
+            $expr = $_GET['expr'];
 
-                // Remove all spaces
-                $expr = str_replace(" ", "", $expr);
+            // Remove all spaces
+            $expr = str_replace(" ", "", $expr);
 
-                // Replace instances of -- with +
-                $expr = str_replace("--", "+", $expr);
+            // Replace instances of -- with +
+            $expr = str_replace("--", "+", $expr);
 
-                // Check for invalid characters
-                $error_expr = '/[^0-9\*\-\+\/]/';
-                if (preg_match($error_expr, $expr)) {
-                        print("Invalid input: ".$expr."\n");
-                        return;
-                }
+            // Check for invalid characters
+            $error_expr = '/[^0-9\*\-\+\/\.]/';
+            if (preg_match($error_expr, $expr)) {
+                    print("Invalid input: ".$expr."\n");
+                    return;
+            }
 
-        $zero = FALSE;
+            $zero = FALSE;
 
-		function error_z($errno, $errstr, $errfile, $errline) {
-		    if ($errstr == "Division by zero") {
-		        global $zero;
-		        $zero = TRUE;
-		    }
-		}
+    		function error_z($errno, $errstr, $errfile, $errline) {
+    		    if ($errstr == "Division by zero") {
+    		        global $zero;
+    		        $zero = TRUE;
+    		    }
+    		}
 
-		set_error_handler('error_z');
+    		set_error_handler('error_z');
 
-        // Handle all the multiplications and divisions
-        $multdiv_pattern = '/\-?\d+\.*\d*(\*|\/)\-?\d+\.*\d*/';
 
-        preg_match($multdiv_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
-        while($matches)
-        {
-                $equation = $matches[0][0];
-                eval("\$result = $equation;");
-                if ($zero == TRUE) { 
-				    echo "Invalid Input: Division by zero error!";
-				    return;
-				}
-                $count = 1;
-                $expr = str_replace($matches[0][0], $result,$expr, $count);
+            // Handle all the multiplications and divisions
+            $multdiv_pattern = '/\-?\d+\.*\d*(\*|\/)\-?\d+\.*\d*/';
 
-                preg_match($multdiv_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);                        
-        }
+            preg_match($multdiv_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
+            while($matches)
+            {
+                    $equation = $matches[0][0];
+                    eval("\$result = $equation;");
+                    if ($zero == TRUE) { 
+    				    echo "Invalid Input: Division by zero error!";
+    				    return;
+    				}
+                    $count = 1;
+                    $expr = str_replace($matches[0][0], $result,$expr, $count);
 
-        // Handle addition and subtraction
-        $addsub_pattern = '/\-?\d+\.*\d*(\+|\-)\-?\d+\.*\d*/';
+                    preg_match($multdiv_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);                        
+            }
 
-        preg_match($addsub_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
+            // Handle addition and subtraction
+            $addsub_pattern = '/\-?\d+\.*\d*(\+|\-)\-?\d+\.*\d*/';
 
-        while($matches)
-        {
-                $equation = $matches[0][0];
-                eval("\$result = $equation;");
-                $count = 1;
-                $expr = str_replace($matches[0][0], $result,$expr, $count);
+            preg_match($addsub_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);
 
-                preg_match($addsub_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);                        
-        }
+            while($matches)
+            {
+                    $equation = $matches[0][0];
+                    eval("\$result = $equation;");
+                    $count = 1;
+                    $expr = str_replace($matches[0][0], $result,$expr, $count);
 
-        print("Answer: ".$result."\n");
+                    preg_match($addsub_pattern, $expr, $matches, PREG_OFFSET_CAPTURE);                        
+            }
+
+            print("Answer: ".$expr."\n");
         }
 ?>
