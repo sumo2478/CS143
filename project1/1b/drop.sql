9a886dff95a84006d24a4ce119e070684fde0961
Alex Guo <aguo91@gmail.com>
2014-01-18 20:31:48 -0800

    Revert "Revert "Implemented project 1b -- create.sql and load.sql""
    This reverts commit f8e5a063b0086be02bcd26888e2b71826a1d1e5b.

---
+++ project1/1b/drop.sql
@@ -0,0 +1,20 @@
+-- NOT TO BE TURNED IN FOR TESTING PURPOSES ONLY
+USE CS143;
+
+DROP TABLE Movie;
+
+DROP TABLE Actor;
+
+DROP TABLE Director;
+
+DROP TABLE MovieGenre;
+
+DROP TABLE MovieDirector;
+
+DROP TABLE MovieActor;
+
+DROP TABLE Review;
+
+DROP TABLE MaxPersonID;
+
+DROP TABLE MaxMovieId;
