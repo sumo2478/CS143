d44aba995938785c16a98b1535c2fe0e172eddb0
Collin Yen <collinyen7@gmail.com>
2014-02-07 00:38:47 -0800

    Implemented search for movies

--- project1/1c/search_results.php
+++ project1/1c/search_results.php
@@ -1,26 +1,43 @@
 <?php 
 	include 'connect.php';
 
-	$query = $_GET['query'];
+	$query = mysql_real_escape_string(trim($_GET['query']));
 
 	// Split the search query into individual words
 	$words = explode(" ", $query);
 
-	$actor_results = array();
+	$actor_results = array(); // List of actors that match query
+	$movie_results = array();
 
 	// For each word in the query select the actor that contains the name in either
 	// his/her first or last name
 	foreach ($words as $word) {
-		$actor_sql_query = "SELECT * FROM Actor WHERE (first LIKE \"%$word%\")
-		OR (last LIKE \"%$word%\")";
+
+		$actor_sql_query = "SELECT id, first, last FROM Actor WHERE (first = \"$word\")
+		OR (last = \"$word\")";
+
+		$movie_sql_query = "SELECT id, title from Movie WHERE title LIKE \"%$word%\"";
 
 		$actor_query_result = mysql_query($actor_sql_query) or die(mysql_error());
+		$movie_query_result = mysql_query($movie_sql_query) or die(mysql_error());
 
+		// Retrieve actor results
 		while ($result = mysql_fetch_assoc($actor_query_result)) {
-			$actor_results[] = $result;
+
+			// If the actor doesn't exist in the array yet then add it in
+			if (!array_key_exists($result['id'], $actor_results)) {
+				$actor_results[$result['id']] = $result;
+			}			
 		}
 
+		// Retrieve movie results
+		while ($result = mysql_fetch_assoc($movie_query_result)) {
+			if (!array_key_exists($result['id'], $movie_results)) {
+				$movie_results[$result['id']] = $result;
+			}
+		}
 	}
+
  ?>
 
 
@@ -30,12 +47,30 @@
 </head>
 <body>
 
-	<h1>Search Results For: <?php echo "\"".$query."\"" ?> </h1>
+	<h1>Search Results For: <?php echo stripslashes($query) ?> </h1>
+
+	<p>Search Actor/Movie: </p>
+
+	<form method="GET">
+		<input type="text" name="query">
+		<input type="submit" value="Search">
+	</form>
+
+	<h2>Actors/Actresses</h2>
 	<?php 
 		if ($query != "") {
 		
 			foreach ($actor_results as $actor) {
 				echo "<p>".$actor['first']." ".$actor['last']."</p>";
+			}
+		}
+	?>
+
+	<h2>Movies</h2>
+	<?php 
+		if ($query != "") {
+			foreach ($movie_results as $movie) {
+				echo "<p>".$movie['title']."</p>";
 			}
 		}
 	?>
