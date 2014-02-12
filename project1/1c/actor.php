a1e14ff6defb0be6bba007f28ad9dc720d0a6151
Collin Yen <collinyen7@gmail.com>
2014-02-11 17:28:24 -0800

    Added more checks to Actor.php and Movie.php

--- project1/1c/actor.php
+++ project1/1c/actor.php
@@ -4,17 +4,20 @@
 	// Retrieve Actor information
 	$actor_id = mysql_real_escape_string(trim($_GET['id']));
 
-	if ($actor_id) {
-		$actor_query = "SELECT * FROM Actor WHERE id = $actor_id";
-		$actor_query_result = mysql_query($actor_query) or die(mysql_error());
+	$actor_query = "SELECT * FROM Actor WHERE id = $actor_id";
+	$actor_query_result = mysql_query($actor_query);		
 
-		$actor = mysql_fetch_assoc($actor_query_result);
+	// Retrieve Movies That Actor Was in
+	$movie_query = "SELECT M.id, M.title, MovieActor.role FROM Actor A, Movie M, MovieActor
+	WHERE A.id = $actor_id AND MovieActor.aid = $actor_id AND MovieActor.mid = M.id";
 
-		// Retrieve Movies That Actor Was in
-		$movie_query = "SELECT M.id, M.title, MovieActor.role FROM Actor A, Movie M, MovieActor
-		WHERE A.id = $actor_id AND MovieActor.aid = $actor_id AND MovieActor.mid = M.id";
-		$movie_query_result = mysql_query($movie_query) or die(mysql_error());
-	}
+	$movie_query_result = mysql_query($movie_query);
+
+	// Check to make sure that we retrieved the actor information
+	if (!$movie_query_result || !$actor_query_result) 
+		die("Failed to retrieve actor information");
+	 
+	 $actor = mysql_fetch_assoc($actor_query_result);
 
  ?>
 
--- project1/1c/movie.php
+++ project1/1c/movie.php
@@ -3,52 +3,57 @@
 
 	// Retrieve Movie information
 	$movie_id = mysql_real_escape_string(trim($_GET['id']));
-	if ($movie_id) {
 
-		$movie_query = "SELECT * FROM Movie WHERE id = $movie_id";
-		$movie_query_result = mysql_query($movie_query) or die(mysql_error());
+	$movie_query = "SELECT * FROM Movie WHERE id = $movie_id";
+	$movie_query_result = mysql_query($movie_query); // Need to check
 
-		$movie = mysql_fetch_assoc($movie_query_result);
+	// Retrieve Genre Information
+	$genre_query = "SELECT genre
+					FROM MovieGenre
+					WHERE mid = $movie_id";
+	$genre_query_result = mysql_query($genre_query); // Need to check
 
-		// Retrieve Genre Information
-		$genre_query = "SELECT genre
-						FROM MovieGenre
-						WHERE mid = $movie_id";
-		$genre_query_result = mysql_query($genre_query) or die(mysql_error());
+	// Retrieve Director Information
+	$director_query = "SELECT D.first, D.last
+					   FROM MovieDirector M, Director D
+					   WHERE M.mid = $movie_id AND D.id=M.did";
+	$director_query_result = mysql_query($director_query); // Need to check
 
-		while ($genre = mysql_fetch_assoc($genre_query_result)) {
-			$genres[] = $genre['genre'];
-		}
+	// Retrieve Actor Information
+	$actor_query = "SELECT A.id, A.last, A.first, MovieActor.role
+					FROM Movie M, Actor A, MovieActor
+					WHERE M.id=$movie_id AND M.id=MovieActor.mid AND A.id=MovieActor.aid";
+	$actor_query_result = mysql_query($actor_query); // Need to check
 
-		// Retrieve Director Information
-		$director_query = "SELECT D.first, D.last
-						   FROM MovieDirector M, Director D
-						   WHERE M.mid = $movie_id AND D.id=M.did";
-		$director_query_result = mysql_query($director_query) or die(mysql_error());
-		
-		while ($director = mysql_fetch_assoc($director_query_result)) {
-			$directors[] = $director['first']." ".$director['last'];
-		}
+	// Retrieve Score Information
+	$score_query = "SELECT AVG(R.rating) AS score
+					FROM Movie M, Review R
+					WHERE M.id=$movie_id AND R.mid=$movie_id";
 
-		// Retrieve Actor Information
-		$actor_query = "SELECT A.id, A.last, A.first, MovieActor.role
-						FROM Movie M, Actor A, MovieActor
-						WHERE M.id=$movie_id AND M.id=MovieActor.mid AND A.id=MovieActor.aid";
-		$actor_query_result = mysql_query($actor_query) or die(mysql_error());
+	$score_query_result = mysql_query($score_query); // Need to check
 
-		// Retrieve Score Information
-		$score_query = "SELECT AVG(R.rating) AS score
-						FROM Movie M, Review R
-						WHERE M.id=$movie_id AND R.mid=$movie_id";
+	// Retrieve Reviews
+	$review_query = "SELECT * FROM Review WHERE mid=$movie_id";
+	$review_query_result = mysql_query($review_query); // Need to check
 
-		$score_query_result = mysql_query($score_query) or die(mysql_error());
-
-		$score = mysql_fetch_assoc($score_query_result);
-
-		// Retrieve Reviews
-		$review_query = "SELECT * FROM Review WHERE mid=$movie_id";
-		$review_query_result = mysql_query($review_query) or die(mysql_error());
+	if (!$movie_query_result    || !$genre_query_result || 
+		!$director_query_result || !$actor_query_result || !$review_query_result) {
+		die("Failed to retrieve movie information");
 	}
+
+
+	// Retrieve Data From Queries
+	$movie = mysql_fetch_assoc($movie_query_result);
+
+	while ($genre = mysql_fetch_assoc($genre_query_result)) {
+		$genres[] = $genre['genre'];
+	}
+
+	while ($director = mysql_fetch_assoc($director_query_result)) {
+		$directors[] = $director['first']." ".$director['last'];
+	}
+
+	$score = mysql_fetch_assoc($score_query_result);
  ?>
 
  <html>
@@ -66,13 +71,10 @@
  	<p>Genres: <?php if ($genres) echo implode(', ', $genres) ?></p>
  	<p>Directors: <?php if ($directors)echo implode(', ', $directors) ?></p>
  	<p>Average User Score: <?php
-
- 								if ($score['score']) {
+ 								if ($score['score'])
  									echo $score['score'];
- 								}else
- 								{
+ 								else 							
  									echo "Not Available";
- 								}
  							?></p>
 
  	<h3>-- Actor Info --</h3>
@@ -85,10 +87,11 @@
  	?>
 
  	<h3>-- Reviews --</h3>
- 	<a href="./add_comment.php?movie_id=<?php echo $movie_id?>">Add Comment</a>
+ 	<a href="./addComment.php?movie_id=<?php echo $movie_id?>">Add Comment</a>
  	<?php 
  		if ($movie_id) {
 	 		while ($review = mysql_fetch_assoc($review_query_result)) {
+	 			echo "<p>--------------------------------------------------</p>";
 	 			echo "<p>".$review['time']."	".$review['name']."</p>";
 	 			echo "<p>Rating: ".$review['rating']."</p>";
 	 			echo "<p>Comment: ".$review['comment']."</p>";
